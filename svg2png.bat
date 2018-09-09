e315447ee9ad62aff527cf1fd1fe29f2224bb1e4
Furrtek <furrtek@gmail.com>
2018-09-09 04:40:04 +0200

    Package defs are now in their own .csv Added more parameters in chip defs Chip body is now opaque Added pin count check DIP/SOIC bugfixes

--- svg2png.bat
+++ svg2png.bat
@@ -1,10 +1,9 @@
 @echo off
-for %%f in (*.svg) do (
+for %%f in (svg\*.svg) do (
     echo %%~f
     "D:\Program Files\Inkscape\inkscape.exe" ^
       -z ^
       --export-background-opacity=0 ^
-      --export-png="%%~dpnf_pinout.png" ^
+      --export-png="png\%%~nf.png" ^
       --file="%%~f"
-
 )
